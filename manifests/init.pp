6286de853e0c7085f01272eca19380c1daa1cf5b
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2012-10-23 15:53:51 +0200

    puppet-linted, and removed c2c-specific code

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,29 +1,25 @@
 class mailman {
-  package {"mailman":
+  package {'mailman':
     ensure => present,
   }
 
-  service {"mailman":
+  service {'mailman':
     ensure     => running,
-    require    => Package["mailman"],
+    require    => Package['mailman'],
     hasstatus  => false,
     hasrestart => true,
     pattern    => '/usr/lib/mailman/bin/mailmanctl -s -q start',
   }
 
-  exec {"mailman set password":
-    command => "/usr/sbin/mmsitepass ree2tahG",
-    creates => "/var/lib/mailman/data/adm.pw",
-    require => Package["mailman"],
+  exec {'mailman set password':
+    command => '/usr/sbin/mmsitepass ree2tahG',
+    creates => '/var/lib/mailman/data/adm.pw',
+    require => Package['mailman'],
   }
-
-  # BUG: I couldn't get this type to work...
-  
-  #maillist {"mailman":
 
   if $mailmanowner {
     $postmaster = $mailmanowner
   } else {
-    $postmaster = "postmaster@camptocamp.com"
+    $postmaster = "postmaster@${::domain}"
   }
 }
